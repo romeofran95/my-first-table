ce5640f7f9bbc1188f3e18bf30ec2cca8c625fe3
francesco <romeofra95@gmail.com>
2022-12-25 10:19:04 +0100

    My Django table, first commit

---
+++ tutorial/models.py
@@ -0,0 +1,8 @@
+# tutorial/models.py
+from django.contrib.auth import get_user_model
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=200,verbose_name="First Name")
+    surname = models.CharField(max_length=200, verbose_name="Last Name")
+    
